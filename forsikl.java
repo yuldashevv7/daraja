fc963883651bc8c25349384bafe5d71f818472a8
yuldashevv7 <muslimbekyuldowev0906@gmail.com>
2025-02-18 09:09:28 +0500

    javascript file

---
+++ forsikl.java
@@ -0,0 +1,7 @@
+public class forsikl {
+    public static void main(String[] args) {
+        for(int i = 50; i <= 60; i++){
+            System.out.println(i +"ning kvadrati =" + (int)Math.pow(i, 2));
+        }
+    }
+}
